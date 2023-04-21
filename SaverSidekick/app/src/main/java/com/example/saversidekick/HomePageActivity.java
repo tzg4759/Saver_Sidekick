826cf11d61531b2b9855933b8788f4706183f9b6
fkaidaw <90433381+fkaidaw@users.noreply.github.com>
2023-04-21 16:39:17 +1200

    Updated app
    Added, Earnings and homepage. updated main so it has a basic welcome page as a starting point.

---
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/HomePageActivity.java
@@ -0,0 +1,36 @@
+package com.example.saversidekick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        // Initialize the UI elements
+        TextView textViewNeeds = findViewById(R.id.textViewNeeds);
+        TextView textViewEverythingElse = findViewById(R.id.textViewEverythingElse);
+        TextView textViewSavings = findViewById(R.id.textViewSavings);
+
+        // Retrieve the weekly earnings from SharedPreferences
+        double weeklyEarnings = PreferenceManager.getDefaultSharedPreferences(this).getFloat("weeklyEarnings", 0);
+
+        // Calculate the amounts for each category
+        double needsAndObligations = weeklyEarnings * 0.5;
+        double everythingElse = weeklyEarnings * 0.3;
+        double savingsAndDebtRepayment = weeklyEarnings * 0.2;
+
+        // Update the UI with the calculated amounts
+        textViewNeeds.setText(String.format(Locale.US, "Needs and obligations: $%.2f", needsAndObligations));
+        textViewEverythingElse.setText(String.format(Locale.US, "Everything else: $%.2f", everythingElse));
+        textViewSavings.setText(String.format(Locale.US, "Savings and debt repayment: $%.2f", savingsAndDebtRepayment));
+    }
+}
