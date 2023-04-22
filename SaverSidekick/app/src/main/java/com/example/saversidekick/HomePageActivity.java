35f45b33cd8e40082632d5c6761c87720e3e3a23
lspain0 <lucasspain@lucasspa.in>
2023-04-22 16:29:15 +1200

    Added goals page
    Added a button on the home page that will navigate to the goals page

--- SaverSidekick/app/src/main/java/com/example/saversidekick/HomePageActivity.java
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/HomePageActivity.java
@@ -2,8 +2,10 @@ package com.example.saversidekick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -19,6 +21,12 @@ public class HomePageActivity extends AppCompatActivity {
         TextView textViewNeeds = findViewById(R.id.textViewNeeds);
         TextView textViewEverythingElse = findViewById(R.id.textViewEverythingElse);
         TextView textViewSavings = findViewById(R.id.textViewSavings);
+
+        Button goalsButton = findViewById(R.id.goalsButton);
+        goalsButton.setOnClickListener(view -> {
+            Intent intent = new Intent(HomePageActivity.this, GoalsActivity.class);
+            startActivity(intent);
+        });
 
         // Retrieve the weekly earnings from SharedPreferences
         double weeklyEarnings = PreferenceManager.getDefaultSharedPreferences(this).getFloat("weeklyEarnings", 0);
