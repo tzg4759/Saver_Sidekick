8e05dc5bf2d7ec97b24519974e40ecdf6e9feafd
Yue LI <yueli.7888@gmail.com>
2023-04-26 12:10:01 +1200

    Create upcoming expense page

---
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/HomePageActivity.java
@@ -0,0 +1,55 @@
+package com.example.saversidekick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        // Initialize the UI elements
+        TextView textViewNeeds = findViewById(R.id.textViewNeeds);
+        TextView textViewEverythingElse = findViewById(R.id.textViewWants);
+        TextView textViewSavings = findViewById(R.id.textViewSavings);
+        TextView textViewEarnings = findViewById(R.id.textViewEarnings);
+        ProgressBar progressBarNecessities = findViewById(R.id.progressBarNecessities);
+        ProgressBar progressBarWants = findViewById(R.id.progressBarWants);
+        ProgressBar progressBarSavings = findViewById(R.id.progressBarSavings);
+
+        Button goalsButton = findViewById(R.id.goalsButton);
+        goalsButton.setOnClickListener(view -> {
+            Intent intent = new Intent(HomePageActivity.this, GoalsActivity.class);
+            startActivity(intent);
+        });
+
+        // Retrieve the weekly earnings from SharedPreferences
+        double weeklyEarnings = PreferenceManager.getDefaultSharedPreferences(this).getFloat("weeklyEarnings", 0);
+
+        // Calculate the amounts for each category
+        double necessities = weeklyEarnings * 0.5;
+        double wants = weeklyEarnings * 0.3;
+        double savings = weeklyEarnings * 0.2;
+
+        // Update the UI with the calculated amounts
+        textViewNeeds.setText(String.format(Locale.US, "Necessities: $%.2f", necessities));
+        textViewEverythingElse.setText(String.format(Locale.US, "Wants: $%.2f", wants));
+        textViewSavings.setText(String.format(Locale.US, "Savings: $%.2f", savings));
+        textViewEarnings.setText((String.format(Locale.US, "Your Earnings: $%.2f", weeklyEarnings)));
+
+        progressBarNecessities.setProgress(50);
+        progressBarWants.setProgress(30);
+        progressBarSavings.setProgress(20);
+
+    }
+}
--- SaverSidekick/app/src/main/java/com/example/saversidekick/MainActivity.java
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.saversidekick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nextButton = findViewById(R.id.earning_button);
+        nextButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, EarningsActivity.class);
+            startActivity(intent);
+        });
+
+        Button viewUpcomingExpense = findViewById(R.id.budge_button);
+        viewUpcomingExpense.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this, UpcomingBudgeActivity.class);
+            startActivity(i);
+        });
     }
+
 }
