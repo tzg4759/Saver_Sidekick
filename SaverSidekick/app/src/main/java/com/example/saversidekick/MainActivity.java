2e1ce129f29678c34e5741c550aafc6620228d00
tzg4759 <tzg4759@autuni.ac.nz>
2023-04-28 18:31:40 +1200

    git test1

--- SaverSidekick/app/src/main/java/com/example/saversidekick/MainActivity.java
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.saversidekick;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+//git test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
