8849d4744d2e73052b73ed107ec1f7f2a19de1a8
Sehun Bang <harrisbang2@gmail.com>
2023-05-03 00:56:02 +1200

    the Savings Search Page is added(to set the period for the graph)

--- SaverSidekick/app/src/main/java/com/example/saversidekick/SavingsGraph.java
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/SavingsGraph.java
@@ -29,16 +29,21 @@ public class SavingsGraph extends AppCompatActivity {
         Intent intent = getIntent();
         name = findViewById(R.id.textView5);
         number = findViewById(R.id.textView6);
-        String getName = intent.getStringExtra("start");
+        String getstart = intent.getStringExtra("start");
         String getNumber = intent.getStringExtra("number");
         //Set Text
+        name.setText(getstart);
+        number.setText(getNumber);
+        int Startnum =Integer.parseInt(getstart)-1;
+        int Monthnum = Integer.parseInt(getNumber);
         // on below line we are initializing our graph view.
         LineChart chart = findViewById(R.id.Graph);
         List<Entry> entries = new ArrayList<Entry>();
         // this will be from the database after it is setup
-        String []finallabel = new String[10];
+        String []finallabel = new String[12];
 
-        for(int i=0;i<=4;i++){
+        for(int i=0;i<=Monthnum-1;i++){
+            finallabel[i]=labels[Startnum+i];
             boolean add = entries.add(new Entry(i, 100));
         }
 
@@ -47,7 +52,7 @@ public class SavingsGraph extends AppCompatActivity {
         XAxis xAxis = chart.getXAxis();
         xAxis.setLabelCount(5); // set the number of labels to display on the X-axis
         xAxis.setGranularity(1f);
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels)); // set the labels for the X-axis
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(finallabel)); // set the labels for the X-axis
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // set the position of the X-axis labels
         LineDataSet dataSet = new LineDataSet(entries, "Label");
         LineData lineData = new LineData(dataSet);
