4c5f9d2ad1cdc492d3a5f274eb2f6c2d6d250840
Sehun Bang <harrisbang2@gmail.com>
2023-06-05 13:36:25 +1200

    CreditCard added, CreditCardInput added

---
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/DisplayCreditCard.java
@@ -0,0 +1,15 @@
+package com.example.saversidekick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayCreditCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_credit_card);
+
+    }
+}
