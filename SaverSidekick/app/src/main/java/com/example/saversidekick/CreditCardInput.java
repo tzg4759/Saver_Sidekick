753d143b44522778101ae78848bc4670ca5eb170
Sehun Bang <harrisbang2@gmail.com>
2023-06-05 14:36:31 +1200

    CreditCard added, CreditCardInput added

--- SaverSidekick/app/src/main/java/com/example/saversidekick/CreditCardInput.java
+++ SaverSidekick/app/src/main/java/com/example/saversidekick/CreditCardInput.java
@@ -10,7 +10,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class CreditCardInput extends AppCompatActivity {
-    EditText cardnumber,Expire,cvv;
+    EditText Cardnumber,Expiredata,cvvnumber;
   //  RadioGroup radioGroup,anz,bnz;
   //  RadioButton radioButton;
     Button add;
@@ -19,6 +19,26 @@ public class CreditCardInput extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit_card_input);
+        //get setting
+       // radioGroup = findViewById(R.id.RadioGroup);
+        Cardnumber=findViewById(R.id.cardnumber);
+        Expiredata=findViewById(R.id.Expire);
+        cvvnumber=findViewById(R.id.cvv);
+        add = findViewById(R.id.add);
+
+        //change to var
+        String getNum = Cardnumber.getText().toString();
+        String date = Expiredata.getText().toString();
+        String getCvv = cvvnumber.getText().toString();
+       
+        //credit card
+       // CreditCard card = new CreditCard(getNum,date,cvv,"anz");
+        //submit
+        add.setOnClickListener(view -> {
+            Intent intent = new Intent(CreditCardInput.this, DisplayCreditCard.class);
+            startActivity(intent);
+        });
+
 
     }
 }
